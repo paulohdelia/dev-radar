9f45c82cf15896ec3540f3f61c4db256e5150d21
paulodelia <paulohdelia@gmail.com>
2020-01-15 18:50:55 -0300

    pasta src adicionada

---
+++ backend/src/controllers/DevControllers.js
@@ -0,0 +1,74 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+// index, show, store, update, destroy
+
+module.exports = {
+    async index(request, response) {
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
+
+
+    async store(request, response) {
+
+        const { github_username, techs , latitude, longitude} = request.body;
+        
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev){
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+        
+            const { name = login, avatar_url, bio } = apiResponse.data;
+        
+            const techsArray = parseStringAsArray(techs);
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+            
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            });
+        }
+    
+        return response.json(dev);
+    },
+
+    async update(request, response) {
+        const { github_username, techs , latitude, longitude, name, bio, avatar_url } = request.body;
+        
+        const techsArray = parseStringAsArray(techs);
+
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        }
+
+        const dev = await Dev.findOneAndUpdate( { github_username } , {
+            name,
+            avatar_url,
+            bio,
+            techs: techsArray,
+            location
+        });
+
+        return response.json(dev);
+
+    },
+
+    async destroy(request, response) {
+        const { github_username } = request.body;
+
+        const dev = await Dev.findOneAndRemove( { github_username } );
+
+        return response.json(dev);
+    }
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const DevController = require('./controllers/DevControllers');
+const SearchController = require('./controllers/SearchController');
+
+const routes = Router();
+
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+routes.put('/devs', DevController.update);
+routes.delete('/devs', DevController.destroy);
+
+routes.get('/search', SearchController.index);
+
+module.exports = routes;
